78b2aaff093a46be6cd6a81bd57151c577735219
bgmellado22 <bgmellado22@gmail.com>
2026-06-04 09:04:37 -0400

    Primer commit

---
+++ src/test/java/com/conectatech/sgs_backend/SgsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conectatech.sgs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
